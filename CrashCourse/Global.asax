dd5d0c1a7aab9bd5e883fe82bd0c81cafd483465
Brennan Hinck <brennanhinck@gmail.com>
2016-09-27 16:33:08 -0400

    Initial Add

---
+++ CrashCourse/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrashCourse.MvcApplication" Language="C#" %>
---
+++ CrashCourse/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrashCourse.Startup))]
+namespace CrashCourse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
